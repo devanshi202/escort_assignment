4ec721fb807dce5956c6ba164391c2ae2671ef9c
Devanshi Sharma <devanshi.ds.20@gmail.com>
2022-02-11 15:17:30 +0530

    assignment update

--- src/Components/SearchFarmer.js
+++ src/Components/SearchFarmer.js
@@ -1,63 +1,73 @@
-import React, { Component } from 'react';
-import "./searchFarmer.css"
-import FarmerDetails from './FarmerDetails';
-import {Link} from "react-router-dom";
-import axios from "axios"
+import React, { Component } from "react";
+import "./searchFarmer.css";
+import FarmerDetails from "./FarmerDetails";
+import { Link } from "react-router-dom";
+import axios from "axios";
 export class SearchFarmer extends Component {
-    constructor(){
-        super();
-        this.state={
-            farmerName:"",
-            name:"",
-            email:"",
-            mobile:"",
-            hasData: false
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      farmerName: "",
+      name: "",
+      email: "",
+      mobile: "",
+      hasData: false,
+    };
+  }
 
-    handleChange=(e)=>{
-        let name = e.target.value;
-        this.setState({
-            farmerName: name
-        })
-    }
+  handleChange = (e) => {
+    let name = e.target.value;
+    this.setState({
+      farmerName: name,
+    });
+  };
 
-    handleClick= async ()=>{
-        let res = await axios.get(`/getFarmerDetails?farmer=${this.state.farmerName}`);
-        console.log(res.data);
-        let data = res.data;
-       this.setState({
-            name: data.name,
-            email: data.email,
-            mobile: data.mobile,
-            hasData: true
-       })  
-    }
+  handleClick = async () => {
+    let res = await axios.get(
+      `/getFarmerDetails?farmer=${this.state.farmerName}`
+    );
+    console.log(res.data);
+    let data = res.data;
+    this.setState({
+      name: data.name,
+      email: data.email,
+      mobile: data.mobile,
+      hasData: true,
+    });
+  };
 
   render() {
-    return <div className='searchFarmerDiv'>
+    return (
+      <div className="searchFarmerDiv">
         <h1>Search Farmer</h1>
-        <input type="text" value={this.state.farmerName} onChange={this.handleChange}></input>  
-        
-        <button type="submit" onClick={this.handleClick}>search</button> 
+        <input
+          type="text"
+          value={this.state.farmerName}
+          onChange={this.handleChange}
+        ></input>
 
-        
-        {this.state.hasData && <div className='farmerDetailDiv'>
-        <div>
-            <h3>Name : </h3>
-            <p>{this.name}</p>
-        </div>
-        <div>
-        <h3>Email : </h3>
-            <p>{this.email}</p>
-        </div>
-        <div>
-        <h3>Mobile : </h3>
-            <p>{this.mobile}</p>
-        </div>
-    </div>}
-       
-    </div>;
+        <button type="submit" onClick={this.handleClick}>
+          search
+        </button>
+
+        {this.state.hasData ?  (
+          <div className="farmerDetail">
+            <div>
+              <h3>Name : </h3>
+              <p>{this.state.name}</p>
+            </div>
+            <div>
+              <h3>Email : </h3>
+              <p>{this.state.email}</p>
+            </div>
+            <div>
+              <h3>Mobile : </h3>
+              <p>{this.state.mobile}</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
