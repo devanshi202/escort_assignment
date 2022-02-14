7e89c8398e5ea1c2f1fb84a35bd614e260851b48
Devanshi Sharma <devanshi.ds.20@gmail.com>
2022-02-14 10:58:40 +0530

    final

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import SearchFarmer from "./Components/SearchFarmer";
 import FarmerDetails from "./Components/FarmerDetails";
 import Queries from "./Components/Queries";
 import RegisterDetail from "./Components/RegisterDetail";
+import Weather from "./Components/Weather";
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
+    <div className="myAppDiv">
+
     <Router>
       <Home />
       <Routes>
-        <Route exact path="/register" element={<Register />}></Route>
+        <Route exact path="/register/*" element={<Register />}></Route>
         <Route path="/search" element={<SearchFarmer />}></Route>
         <Route path="/queries" element={<Queries />} />
-        
+        <Route path="/weather" element={<Weather />} />
        
       </Routes>
     </Router>
+    </div>
   );
 }
 
